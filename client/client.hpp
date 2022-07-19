a692633bbec27275e50b1d20e240e0add3e21be7
VictorAldica <victor.aldica.official@gmail.com>
2022-07-19 14:42:12 +0300

    Socket test

--- client/client.hpp
+++ client/client.hpp
@@ -1,10 +1,45 @@
 #pragma once
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
 
 class Client {
 public:
   Client();
   ~Client();
+  void CreateSocket() {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      printf("\n Socket creation error \n");
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+  }
+  void Connect() {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+      printf("\nInvalid address/ Address not supported \n");
+    }
+
+    if ((client_fd = connect(sock, (struct sockaddr *)&serv_addr,
+                             sizeof(serv_addr))) < 0) {
+      printf("\nConnection Failed \n");
+    }
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+  }
+  void Close() { close(client_fd); }
 
 private:
-
+  int sock = 0, valread, client_fd;
+  struct sockaddr_in serv_addr;
+  char *hello = "Hello from client";
+  char buffer[1024] = {0};
 };
+
+
+//to be reworked
