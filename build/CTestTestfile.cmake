59c5af5490a3cb58768f6fd54c80c34cd8fbc1f5
VictorAldica <victor.aldica.official@gmail.com>
2022-07-22 01:58:53 +0300

    Main demo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/victor/Documents/Siemens/Project-Thermostat-Valve
-# Build directory: /home/victor/Documents/Siemens/Project-Thermostat-Valve/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
